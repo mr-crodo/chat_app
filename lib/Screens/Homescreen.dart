e940068f0fd5d801bc17c643e6f7b0ada36372f0
Nasib Mammadov <bduti09@gmail.com>
2026-02-09 12:32:59 +0400

    new full stack chat aplication

---
+++ lib/Screens/Homescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Whatsapp Clone"),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: null)],
+      ),
+    );
+  }
+}
