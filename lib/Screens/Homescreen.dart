a38e62a81439890e9dad6d30c22da3bf23c032d2
Nasib Mammadov <bduti09@gmail.com>
2026-02-09 17:57:52 +0400

    chat page finish

--- lib/Screens/Homescreen.dart
+++ lib/Screens/Homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Pages/ChatPage.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -7,13 +8,61 @@ class Homescreen extends StatefulWidget {
   State<Homescreen> createState() => _HomescreenState();
 }
 
-class _HomescreenState extends State<Homescreen> {
+class _HomescreenState extends State<Homescreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    // Todo: implement initState
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Whatsapp Clone"),
-        actions: [IconButton(icon: Icon(Icons.search), onPressed: null)],
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(child: Text("New group"), value: "New group"),
+                PopupMenuItem(
+                  child: Text("New broadcast"),
+                  value: "New broadcast",
+                ),
+                PopupMenuItem(child: Text("Whatsap Web"), value: "Whatsap Web"),
+                PopupMenuItem(
+                  child: Text("Starred messages"),
+                  value: "Starred messages",
+                ),
+                PopupMenuItem(child: Text("Settings"), value: "Settings"),
+                PopupMenuItem(child: Text("Logout"), value: "Logout"),
+              ];
+            },
+          ),
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          indicatorColor: Colors.white,
+          labelColor: Colors.white, // активRная вкладка
+          unselectedLabelColor: Colors.white70, // неактивные
+          tabs: [
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(text: "STATUS"),
+            Tab(text: "CALLS"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [Text("Camera"), ChatPage(), Text("Calls"), Text("Status")],
       ),
     );
   }
