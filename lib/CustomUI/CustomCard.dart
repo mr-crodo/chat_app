e7ee99357e3bdb858885849298ec587f866e18ec
Nasib Mammadov <bduti09@gmail.com>
2026-02-11 10:47:43 +0400

    lesson 6 finish

--- lib/CustomUI/CustomCard.dart
+++ lib/CustomUI/CustomCard.dart
@@ -1,24 +1,48 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 
 class CustomCard extends StatelessWidget {
   const CustomCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(radius: 30),
-      title: Text(
-        "John Doe",
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      subtitle: Row(
+    return InkWell(
+      onTap: () {},
+      child: Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(width: 3),
-          Text("Hi, Dev Stack! How are you!", style: TextStyle(fontSize: 14)),
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: SvgPicture.asset(
+                "assets/groups.svg",
+                color: Colors.white,
+                width: 38,
+                height: 38,
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            title: Text(
+              "John Doe",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(width: 3),
+                Text(
+                  "Hi, Dev Stack! How are you!",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+            trailing: Text("18:08"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, left: 80),
+            child: Divider(thickness: 1),
+          ),
         ],
       ),
-      trailing: Text("18:08"),
     );
   }
 }
