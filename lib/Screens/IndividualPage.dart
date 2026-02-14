591d9bb161ae82031260e935dcd329802c4b8350
Nasib Mammadov <bduti09@gmail.com>
2026-02-14 13:51:58 +0400

    new fd

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -1,3 +1,4 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:chat_app/Model/ChatModel.dart';
@@ -11,9 +12,25 @@ class IndividualPage extends StatefulWidget {
 }
 
 class _IndividualPageState extends State<IndividualPage> {
+  bool show = false;
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          show = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         // leadingWidth: 70,
         titleSpacing: 0, // убираем стандартный отступ слева у title
@@ -34,24 +51,28 @@ class _IndividualPageState extends State<IndividualPage> {
               ),
             ),
             const SizedBox(width: 10),
-            Container(
-              margin: EdgeInsets.all(5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.chatModel.name,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.chatModel.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const Text(
-                    "Last seen today at 12:05",
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ],
+                    const Text(
+                      "Last seen today at 12:05",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 13),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -67,10 +88,145 @@ class _IndividualPageState extends State<IndividualPage> {
             style: IconButton.styleFrom(foregroundColor: Colors.white),
             onPressed: () {},
           ),
-          PopupMenuButton(itemBuilder: (context) => []),
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.more_vert), // Вертикальные точки
+            offset: const Offset(0, 50), // Появляется под AppBar
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: "View Contact",
+                  child: Text("View Contact"),
+                ),
+                const PopupMenuItem(
+                  value: "Media, links, and docs",
+                  child: Text("Media, links, and docs"),
+                ),
+                const PopupMenuItem(
+                  value: "Whatsapp Web",
+                  child: Text("Whatsapp Web"),
+                ),
+                const PopupMenuItem(value: "Search", child: Text("Search")),
+                const PopupMenuItem(
+                  value: "Mute notifications",
+                  child: Text("Mute notifications"),
+                ),
+                const PopupMenuItem(
+                  value: "Wallpaper",
+                  child: Text("Wallpaper"),
+                ),
+              ];
+            },
+          ),
         ],
       ),
-      body: const SizedBox.shrink(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: PopScope(
+          child: Stack(
+            children: [
+              ListView(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width - 60,
+                          child: Card(
+                            margin: EdgeInsets.only(
+                              left: 2,
+                              right: 2,
+                              bottom: 8,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: TextFormField(
+                              focusNode: focusNode,
+                              textAlignVertical: TextAlignVertical.center,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 5,
+                              minLines: 1,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Type a message",
+                                prefixIcon: IconButton(
+                                  icon: const Icon(
+                                    Icons.emoji_emotions_outlined,
+                                    color: Colors.blueGrey,
+                                  ),
+                                  onPressed: () {
+                                    focusNode.unfocus();
+                                    focusNode.canRequestFocus = false;
+                                    setState(() {
+                                      show = !show;
+                                    });
+                                  },
+                                ),
+                                suffixIcon: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(
+                                        Icons.attach_file,
+                                        color: Colors.blueGrey,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(
+                                        Icons.camera_alt,
+                                        color: Colors.blueGrey,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                                contentPadding: const EdgeInsets.all(5),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 8.0,
+                            right: 5,
+                            left: 2,
+                          ),
+                          child: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Color(0xFF128C7E),
+                            child: IconButton(
+                              icon: Icon(Icons.mic, color: Colors.white),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    show ? emojiSelect() : Container(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget emojiSelect() {
+    return EmojiPicker(
+      onEmojiSelected: (emoji, category) {
+        print(emoji);
+      },
+      config: Config(emojiViewConfig: const EmojiViewConfig(columns: 7)),
     );
   }
 }
